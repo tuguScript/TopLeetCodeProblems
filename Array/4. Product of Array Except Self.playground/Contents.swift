fd7d5971d83024763dd93fe1b68f8a20e595347e
tuguscript <tuguscript@gmail.com>
2021-08-11 16:56:33 -0700

    added new problem

--- Array/4. Product of Array Except Self.playground/Contents.swift
+++ Array/4. Product of Array Except Self.playground/Contents.swift
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/product-of-array-except-self/
 
 class Solution {
+//    Brute force approach
     func productExceptSelf(_ nums: [Int]) -> [Int] {
         var tempArray:[Int] = []
         for i in nums.indices{
@@ -17,8 +18,25 @@ class Solution {
         }
         return tempArray
     }
+//    Approach 1: Left and Right product lists
+    func nameproductExceptSelfLeftRight(_ nums: [Int]) -> [Int] {
+        var answer = Array(repeating: 1, count: nums.count)
+        var prod = 1
+        for i in answer.indices {
+            answer[i] = prod
+            prod = prod * nums[i]
+        }
+        prod = 1
+        for i in (0..<nums.count).reversed() {
+            print(i)
+            answer[i] = answer[i] * prod
+            prod = prod * nums[i]
+        }
+        return answer
+    }
 }
 
 var x = Solution()
 var nums = [1,2,3,4]
-x.productExceptSelf(nums)
+//var nums = [4,5,1,8,2,10,6]
+x.nameproductExceptSelfLeftRight(nums)
